99bbb153b456770a79ca02b83b8556f9ea36f5bb
Giao Ngo <giao.ngo@metropolia.fi>
2023-02-06 12:20:20 +0200

    refactor: refactor favourite features's code for CardIconButton component of Home Screen

--- components/CardIconButton.js
+++ components/CardIconButton.js
@@ -13,7 +13,7 @@ const CardIconButton = ({dataId}) => {
 
   const [averageRating, setAverageRating] = useState([]);
   const [clickStar, setClickStar] = useState(false);
-  const [update, setUpdate] = useState(false);
+
   const {loadFavouritesByFileId, addFavourite, removeFavourite} =
     useFavourite();
   const {loadCommentsByFileId} = useComment();
@@ -21,35 +21,49 @@ const CardIconButton = ({dataId}) => {
 
   // Hard codeded token and user id. This part will be replaced by fetching user data from main context and AsyncStorage
   const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyODYzLCJ1c2VybmFtZSI6InBodW9uZ2dpYW8iLCJlbWFpbCI6ImdpYW8ubmdvQG1ldHJvcG9saWEuZmkiLCJmdWxsX25hbWUiOm51bGwsImlzX2FkbWluIjpudWxsLCJ0aW1lX2NyZWF0ZWQiOiIyMDIzLTAyLTA0VDE4OjM0OjExLjAwMFoiLCJpYXQiOjE2NzU2MDgwMDcsImV4cCI6MTY3NTY5NDQwN30.sousYuHuTFLpQ4oJ082GKpf-eS9LGZkRc-_vQCxt2Bw';
-
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyODYzLCJ1c2VybmFtZSI6InBodW9uZ2dpYW8iLCJlbWFpbCI6ImdpYW8ubmdvQG1ldHJvcG9saWEuZmkiLCJmdWxsX25hbWUiOm51bGwsImlzX2FkbWluIjpudWxsLCJ0aW1lX2NyZWF0ZWQiOiIyMDIzLTAyLTA0VDE4OjM0OjExLjAwMFoiLCJpYXQiOjE2NzU2Nzc5MDMsImV4cCI6MTY3NTc2NDMwM30.8oi_2f0Z_eNuPstjwJH_0S85QmSDz14KIlYEeAO5hzU';
   const userId = 2863;
-  useEffect(() => {
-    loadFavouritesByFileId(dataId).then((favourites) => {
-      setFavouriteArray(favourites);
-      const checkFavourite = favourites.some(
-        (favourite) => favourite.user_id === userId
-      );
-      setIsFavourite(checkFavourite);
-    });
-  }, [update]);
+
+  const getFavouritesByFileId = async () => {
+    const favourites = await loadFavouritesByFileId(dataId);
+    setFavouriteArray(favourites);
+    const checkFavourite = favourites.some(
+      (favourite) => favourite.user_id === userId
+    );
+    setIsFavourite(checkFavourite);
+  };
+
+  const getCommentsByFileId = async () => {
+    const comments = await loadCommentsByFileId(dataId);
+    setCommentArray(comments);
+  };
+
+  const getRatingsByFileId = async () => {
+    const ratings = await loadRatingsByFileId(dataId);
+    const ratingOnlyArray = ratings.map((element) => element.rating);
+    setAverageRating(
+      ratingOnlyArray.length &&
+        ratingOnlyArray.reduce((prev, cur) => prev + cur, 0) /
+          ratingOnlyArray.length
+    );
+  };
+
+  const likeFile = async () => {
+    const result = await addFavourite(dataId, token);
+    console.log('result of adding favourite', result);
+    getFavouritesByFileId();
+  };
+
+  const dislikeFile = async () => {
+    const result = await removeFavourite(dataId, token);
+    console.log('result of removing favourite', result);
+    getFavouritesByFileId();
+  };
 
   useEffect(() => {
-    loadCommentsByFileId(dataId).then((comments) => {
-      setCommentArray(comments);
-    });
-  }, []);
-
-  useEffect(() => {
-    loadRatingsByFileId(dataId).then((ratings) => {
-      const ratingOnlyArray = ratings.map((element) => element.rating);
-
-      setAverageRating(
-        ratingOnlyArray.length &&
-          ratingOnlyArray.reduce((prev, cur) => prev + cur, 0) /
-            ratingOnlyArray.length
-      );
-    });
+    getFavouritesByFileId();
+    getCommentsByFileId();
+    getRatingsByFileId();
   }, []);
 
   return (
@@ -58,12 +72,8 @@ const CardIconButton = ({dataId}) => {
         icon={isFavourite ? 'heart' : 'heart-outline'}
         size={30}
         iconColor="#E0E0E0"
-        onPress={async () => {
-          const result = !isFavourite
-            ? await addFavourite(dataId, token)
-            : await removeFavourite(dataId, token);
-          console.log('result of favourite', result);
-          setUpdate(!update);
+        onPress={() => {
+          !isFavourite ? likeFile() : dislikeFile();
         }}
       />
       <Text>{favouriteArray.length || 0}</Text>
