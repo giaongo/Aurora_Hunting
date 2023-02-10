99856bbe3ea6fd80dd73314b170451aa1b854637
Giao Ngo <giao.ngo@metropolia.fi>
2023-02-10 23:38:14 +0200

    fix: comment out the bug in CommentItem to avoid the app crash

--- components/CommentItem.js
+++ components/CommentItem.js
@@ -1,12 +1,18 @@
-import { Avatar, Card} from "react-native-paper";
-import React, { useContext, useEffect, useState } from 'react';
+import {Avatar, Card} from 'react-native-paper';
+import React, {useContext, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
-import { useMedia, useTag, useUser } from "../hooks/ApiHooks";
-import { uploadsUrl } from "../utils/variables";
-import { Alert, StyleSheet, View, Text } from "react-native";
-import { ButtonGroup, Divider } from '@rneui/themed';
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { MainContext } from "../contexts/MainContext";
+import {useMedia, useTag, useUser} from '../hooks/ApiHooks';
+import {uploadsUrl} from '../utils/variables';
+import {Alert, StyleSheet, View, Text} from 'react-native';
+// -------------------------------------------------------------------------------------------------
+// import { ButtonGroup, Divider } from '@rneui/themed';
+
+// BIG NOTE: We have bug by this code Tai.
+// Rneui/theme is for react native element and we dont use this library. Change to react native paper instead.
+
+// ----------------------------------------------------------------------------
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {MainContext} from '../contexts/MainContext';
 
 const CommentItem = ({data}) => {
   const [avatar, setAvatar] = useState('');
@@ -16,7 +22,7 @@ const CommentItem = ({data}) => {
   const {getUserById} = useUser();
   const {deleteMedia} = useMedia();
 
-  const loadAvatar = async() => {
+  const loadAvatar = async () => {
     try {
       const tag = 'avatar_' + data.user_id;
       const files = await getFilesByTag(tag);
@@ -26,7 +32,7 @@ const CommentItem = ({data}) => {
     }
   };
 
-  const getUsernameById = async() => {
+  const getUsernameById = async () => {
     try {
       const token = await AsyncStorage.getItem('userToken');
       const usernames = await getUserById(data.user_id, token);
@@ -34,46 +40,45 @@ const CommentItem = ({data}) => {
     } catch (error) {
       console.error('getUsernameById: ', error);
     }
-  }
+  };
 
   const doDelete = () => {
     try {
       Alert.alert('Delete', 'this file permanently ?', [
-        {text:'Cancel'},
+        {text: 'Cancel'},
         {
-          text:'Yes',
-          onPress: async() => {
+          text: 'Yes',
+          onPress: async () => {
             const token = await AsyncStorage.getItem('userToken');
             const response = await deleteMedia(token, data.comment_id);
             response && setUpdate(!update);
-            response ? Alert.alert('Deleted Comment successfully') : Alert.alert('There is something wrong')
+            response
+              ? Alert.alert('Deleted Comment successfully')
+              : Alert.alert('There is something wrong');
           },
         },
       ]);
     } catch (error) {
       console.error('doDelele: ', error);
     }
-  }
+  };
 
   useEffect(() => {
     loadAvatar();
     getUsernameById();
-  }, [])
+  }, []);
 
   return (
     <View style={styles.container}>
       <Avatar.Image
         size={100}
         source={{
-          uri: avatar ? uploadsUrl  + avatar : 'https://placedog.net/500'
-          }}
+          uri: avatar ? uploadsUrl + avatar : 'https://placedog.net/500',
+        }}
         style={styles.avatar}
       />
       <View style={styles.contentContainer}>
-        <Card.Title
-          title={username.username}
-          style={styles.cardTitle}
-        />
+        <Card.Title title={username.username} style={styles.cardTitle} />
         <Text style={styles.commentContent}>{data.comment}</Text>
         <Text style={styles.commentDate}>{data.time_added.split('T')[0]}</Text>
       </View>
@@ -84,59 +89,57 @@ const CommentItem = ({data}) => {
         buttons={['Modify', 'Delete']}
         rounded
         onPress={(index) => {
-            if(index === 0){
-              console.log('pressed modify');
-            } else {
-              doDelete();
-            }
+          if (index === 0) {
+            console.log('pressed modify');
+          } else {
+            doDelete();
+          }
         }}
       />
-
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     backgroundColor: '#212121',
-    flexDirection:'row',
+    flexDirection: 'row',
     paddingTop: 20,
   },
   avatar: {
-    borderColor:'green',
-    borderWidth:2,
-    borderStyle:'dotted'
+    borderColor: 'green',
+    borderWidth: 2,
+    borderStyle: 'dotted',
   },
-  contentContainer:{
-    flexDirection:'column',
-    width:'50%',
+  contentContainer: {
+    flexDirection: 'column',
+    width: '50%',
   },
-  cardTitle:{
-    alignItems:'flex-start'
+  cardTitle: {
+    alignItems: 'flex-start',
   },
   commentContent: {
-    marginLeft:20,
-    marginBottom:10,
-    fontSize:20,
-    color:'#F0FFFF'
+    marginLeft: 20,
+    marginBottom: 10,
+    fontSize: 20,
+    color: '#F0FFFF',
   },
-  commentDate:{
-    marginLeft:20,
-    marginTop:10,
-    fontSize:10,
-    color:'#F0F8FF',
-    height:'100%'
+  commentDate: {
+    marginLeft: 20,
+    marginTop: 10,
+    fontSize: 10,
+    color: '#F0F8FF',
+    height: '100%',
   },
-  buttonContainer:{
-    height:150,
-    width:'20%',
-    flexDirection:'column',
+  buttonContainer: {
+    height: 150,
+    width: '20%',
+    flexDirection: 'column',
   },
-  button:{
-    borderBottomWidth:1
-  }
-
+  button: {
+    borderBottomWidth: 1,
+  },
 });
 
 CommentItem.propTypes = {
