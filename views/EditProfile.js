f65c42096c515ad2b4f1d99b5800bb906ce977d9
tain <phattai2407@gmail.com>
2023-02-16 10:10:54 +0200

    fixing bugs in loadAvatar and loadWallPaper

--- views/EditProfile.js
+++ views/EditProfile.js
@@ -48,7 +48,7 @@ const EditProfile = ({route}) => {
     try {
       const tag = 'avatar_' + user.user_id;
       const files = await getFilesByTag(tag);
-      setUserAvatar(files?.pop().filename);
+      setUserAvatar(files?.pop()?.filename);
     } catch (error) {
       console.error('loadAvatar: ', error);
     };
@@ -58,7 +58,7 @@ const EditProfile = ({route}) => {
     try {
       const tag = 'wallpaper_' + user.user_id;
       const files = await getFilesByTag(tag);
-      setUserWallPaper(files?.pop().filename);
+      setUserWallPaper(files?.pop()?.filename);
     } catch (error) {
       console.error('loadAvatar: ', error);
     };
