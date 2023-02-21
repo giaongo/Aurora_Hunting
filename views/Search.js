29df10a9c4bda2aa2236b25a7ae0dc2fbc6d0727
Ainara Larrañaga <ainara.larranagaflores@metropolia.fi>
2023-02-21 20:06:13 +0200

    search view

--- views/Search.js
+++ views/Search.js
@@ -1,19 +1,80 @@
-import React from 'react';
-import {View} from 'react-native';
-import {Text} from 'react-native-paper';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {TextInput} from 'react-native-paper';
 import PropTypes from 'prop-types';
+import {useMedia} from '../hooks/ApiHooks';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useEffect} from 'react';
+import CardItem from '../components/CardItem';
 
-const Search = (navigation) => {
+const Search = () => {
+  const [searchFile, setSearchFile] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const {searchMedia} = useMedia();
+
+  const handleSearch = async () => {
+    try {
+      const userToken = AsyncStorage.getItem('userToken');
+      setLoading(true);
+      const results = await searchMedia(searchFile, userToken);
+      console.log('searchFile', results);
+      setSearchResults(results);
+    } catch (error) {
+      console.error('search media failed', error);
+    }
+  };
+
+  // const {mediaArray, loadMedia, searchMedia} = useMedia();
+
+  // const searchFile = async (navigation) => {
+  //   try {
+  //     const userToken = AsyncStorage.getItem('userToken');
+  //     const result = await searchMedia(userToken);
+  //     console.log('searchFile', result);
+  //   } catch (error) {
+  //     console.error('search media failed', error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   searchFile();
+  // }, []);
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Search for media"
+        placeholderTextColor={'Grey'}
+        value={searchFile}
+        onChangeText={(text) => setSearchFile(text)}
+        onSubmitEditing={handleSearch}
+        loading={loading}
+      />
+      {searchResults.map((item) => (
+        <CardItem key={item.id} title={item.title} />
+      ))}
     </View>
   );
 };
 
 Search.propTypes = {
-  route: PropTypes.object,
   navigation: PropTypes.object,
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    alignContent: 'flex-start',
+    paddingTop: 50,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20,
+  },
+});
 
 export default Search;
