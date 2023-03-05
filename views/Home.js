e2eef92bbbe40ae9ca8b55baf8bef396ce84853c
tain <phattai2407@gmail.com>
2023-03-05 11:51:30 +0200

    added scrolltoTop and refreshIndicator

--- views/Home.js
+++ views/Home.js
@@ -1,10 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {FlatList, StyleSheet, View} from 'react-native';
 import CardItem from '../components/CardItem';
 import {useMedia} from '../hooks/ApiHooks';
 import PropTypes from 'prop-types';
 import {Toast} from 'react-native-toast-message/lib/src/Toast';
+import {useState} from 'react';
+import {useCallback} from 'react';
+import {useContext} from 'react';
+import {MainContext} from '../contexts/MainContext';
+import {RefreshControl} from 'react-native';
+import {useScrollToTop} from '@react-navigation/native';
+
 const Home = ({navigation}) => {
+  const {update, setUpdate} = useContext(MainContext);
+  const [refreshing, setRefreshing] = useState(false);
+  const ref = useRef(null);
+  useScrollToTop(ref);
   const {mediaArray} = useMedia();
 
   const showToast = (message1, message2) => {
@@ -18,6 +29,14 @@ const Home = ({navigation}) => {
     });
   };
 
+  const onRefresh = useCallback(() => {
+    setUpdate(!update);
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
   useEffect(() => {
     showToast('Click on location tag', 'for weather information');
   }, []);
@@ -25,6 +44,15 @@ const Home = ({navigation}) => {
   return (
     <View style={styles.container}>
       <FlatList
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            tintColor={'azure'}
+            color={'azure'}
+          />
+        }
+        ref={ref}
         data={mediaArray}
         keyExtractor={(item) => item.file_id}
         renderItem={({item}) => (
